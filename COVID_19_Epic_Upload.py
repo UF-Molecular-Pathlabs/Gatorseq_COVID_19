b9869e4261f63d6e74318c6d1678e53174e56b94
Johnny Bravo <HaKmonkey@gmail.com>
2022-02-21 19:41:35 -0500

    Created COVID_19_module
    Moved the 2 classes in the script and their associated functions into their own 'module'. Will re-organize some of the code into a main function now, then probably add some doc strings for clarity.

--- COVID_19_Epic_Upload.py
+++ COVID_19_Epic_Upload.py
@@ -11,6 +11,7 @@ import os.path
 import re
 import seaborn as sns
 import matplotlib.pyplot as plt
+import COVID_19_module
 
 print("Run start time: ", str(datetime.datetime.now()) + "\n")
 
@@ -34,6 +35,7 @@ def replace_env(strname):
     strname=strname.replace("USER_NAME",USER_NAME).replace("CODE_ENV",CODE_ENV)
     return strname
 
+
 COVID_19_EPIC_UPLOAD_TABLE = replace_env(config_dict['COVID_19_EPIC_UPLOAD_TABLE'])
 COVID_19_TEST_INPUT_FOLDER = replace_env(config_dict['COVID_19_TEST_INPUT_FOLDER']) #'G:\DRL\Molecular\Assays\PGX\PGX_Beaker_Interface' 
 COVID_19_TEST_SAMPLE_LOG = replace_env(config_dict['COVID_19_TEST_SAMPLE_LOG'])
@@ -45,7 +47,6 @@ if CODE_ENV=='ProdEnv':
     COVID_19_TEST_INPUT_FOLDER = replace_env(config_dict['COVID_19_TEST_INPUT_FOLDER_PROD']) 
 else:
     MIRTH_GATORSEQ += '/TEST'
-
 
 
 config_token_dict=dict()
@@ -77,105 +78,6 @@ def check_folders_exist():
 
 
 check_folders_exist()
-def format_for_unity(x):
-    if(x<10):
-        return "0"+str(x)
-    else:
-        return str(x)
-
-def get_current_formatted_date():
-    currentDT = datetime.datetime.now()
-    if currentDT:
-        data = format_for_unity(currentDT.year) +format_for_unity(currentDT.month) +format_for_unity(currentDT.month)+format_for_unity(currentDT.hour)+format_for_unity(currentDT.minute)+format_for_unity(currentDT.second)
-        
-        return data
-    return str(currentDT)
-
-def get_ticks(dt):
-    return (dt - datetime.datetime(1, 1, 1)).total_seconds() * 10000000
-
-class hl7update:
-    
-    def __init__(self, h):
-        self.h = h
-
-    def update_msh_segment(self):
-        if self.h and self.h['MSH']:
-            for msh_segment in self.h['MSH']:
-                if msh_segment:
-                    msh_segment[7] = get_current_formatted_date()
-                    msh_segment[8] = ''
-                    msh_segment[9][0][0] = 'ORU'
-                    msh_segment[9][0][1] = 'R01'
-                    msh_segment[10] = get_ticks(datetime.datetime.now())
-
-    def update_orc_segment(self):
-        if self.h and self.h['ORC']:
-            for orc_segment in self.h['ORC']:
-                orc_segment[1] = 'RE'
-
-    def update_obr_segment(self):
-        if self.h and self.h['OBR']:
-            for obr_segment in self.h['OBR']:
-                obr_segment[22] = get_current_formatted_date()
-                obr_segment[25] = 'P'
-                obr_segment[27] = '^^^^^R^^'
-
-    def update_obx_segment(self):
-        if self.h and self.h['OBX']:
-            for obx_segment in self.h['OBX']:
-                obx_segment[2] = 'ST'
-                obx_segment[11] = 'P'
-                obx_segment[14] = get_current_formatted_date()
-                if(len(obx_segment)==19):
-                    obx_segment.append(obx_segment[14])
-                elif(len(obx_segment)>=19):
-                    obx_segment[19] = obx_segment[14]
-   
-    def update_obx_seg_containing_gene(self, sample):
-        updates = 0
-        temp_obx = self.h[:]
-        l = len(self.h)
-        for i in range(l):
-            del temp_obx[l-i-1]
-        new_obx_index = 1
-        for obxSegment in self.h['OBX']:
-            if obxSegment[3][0][1][0] == "SARS-COV-2, NAA":
-                obxSegment[5][0] = sample.result
-                obxSegment[1] = new_obx_index
-                obxSegment[18][0] = "Genome"
-                new_obx_index +=1 
-                temp_obx.append(obxSegment) 
-            
-        h_t = self.h[:]
-        l = len(self.h)
-        for i in range(l):
-            del h_t[l-i-1]
-        for i in range(len(self.h)):
-            if(self.h[i][0][0]!="OBX"):
-                h_t.append(self.h[i])
-        h_t.extend(temp_obx)
-        return h_t
-
-    def get_first_obx_index(self):
-        idx = 0
-        for seg in self.h:
-            if seg[0][0] == 'OBX':
-                return idx
-            idx += 1
-        return -1
-    # Assuming insertion is just above first OBX segment
-    def update_comments(self, comments):
-        comments_arr = comments.split("\n")
-        obx_idx = self.get_first_obx_index()
-        if (obx_idx == -1):
-            print("OBX segment not found, so appending it")
-            obx_idx = len(self.h) - 1 
-        i=1
-        for comment in comments_arr:
-            self.h.append('NTE|{}|L|{}'.format(i,comment))
-            obx_idx += 1
-            i += 1
 
 
 def create_connection():
@@ -269,7 +171,7 @@ def checkIncomingHl7(sampleDict, commentsDict, excelFile):
             if hl7msg: #check if message not empty
                 msg_unix_fmt = hl7msg.replace("\n","\r")
                 h = hl7.parse(msg_unix_fmt)
-                newHl7 = hl7update(h)
+                newHl7 = COVID_19_module.hl7update(h)
                 #Read message id from HL7 message
                 try:
                     messageId = str(h['OBR'][0][3]).replace("^", "")
@@ -339,26 +241,10 @@ def checkIncomingHl7(sampleDict, commentsDict, excelFile):
                         with open(out_file_path, 'w' ,  encoding='utf-8') as f:
                             f.write(str(h))
                         print("Out file available at :",out_file_path)
-                        move(ORDERS_DIR + hl7_file_name, ORDERS_ARCHIVE_DIR + 'COVID_19_processed_' + get_current_formatted_date() + "-" + hl7_file_name) 
+                        move(ORDERS_DIR + hl7_file_name, ORDERS_ARCHIVE_DIR + 'COVID_19_processed_' + COVID_19_module.get_current_formatted_date() + "-" + hl7_file_name) 
                         if plm:
                             updateRowInDatabase(givenSample, plm, str(mrn), str(ptName), str(ptSex), str(ptAge), str(ordDept), excelFile )
                     
-
-class Sample:
-    def __init__(self, sample_name, completeSampleName):
-        self.name = str(sample_name)
-        self.nCoV_N1 = None
-        self.nCoV_N2 = None
-        self.nCoV_N3 = None
-        self.RP = None
-        self.result = None
-        self.completeSampleName = completeSampleName
-
-    def __str__(self):
-        return str( self.completeSampleName) + ": " + str(self.nCoV_N1) + " & " + str(self.nCoV_N2) + " & "  + str(self.nCoV_N3) + " & " + str(self.RP) + " & " + str(self.result)
-
-    def __repr__(self):
-        return str( self.completeSampleName) + ": " + str(self.nCoV_N1) + " & " + str(self.nCoV_N2) + " & "  + str(self.nCoV_N3) + " & " + str(self.RP) + " & " + str(self.result) + " | "
 
 def isFloatValue(value, maxThreshold):
     try:
@@ -566,7 +452,7 @@ if __name__ == "__main__":
                 
                 #ToDo: sampleName = <PLMO of a sample> or <id number of a sample>
                 if sampleDict.get(sampleName) is None:
-                    sampleDict[sampleName] = Sample(sampleName, str(row["Sample Name"]))
+                    sampleDict[sampleName] = COVID_19_module.Sample(sampleName, str(row["Sample Name"]))
                 if targetName == "RP":# and not math.isnan(value):
                     setattr(sampleDict[sampleName], "%s" % targetName, value)
                 else:
---
+++ COVID_19_module.py
@@ -0,0 +1,119 @@
+import datetime
+
+
+def format_for_unity(x):
+    if(x<10):
+        return "0"+str(x)
+    else:
+        return str(x)
+
+
+def get_current_formatted_date():
+    currentDT = datetime.datetime.now()
+    if currentDT:
+        data = format_for_unity(currentDT.year) +format_for_unity(currentDT.month) +format_for_unity(currentDT.month)+format_for_unity(currentDT.hour)+format_for_unity(currentDT.minute)+format_for_unity(currentDT.second)
+        return data
+    return str(currentDT)
+
+
+def get_ticks(dt):
+    return (dt - datetime.datetime(1, 1, 1)).total_seconds() * 10000000
+
+
+class hl7update:
+    def __init__(self, h):
+        self.h = h
+
+    def update_msh_segment(self):
+        if self.h and self.h['MSH']:
+            for msh_segment in self.h['MSH']:
+                if msh_segment:
+                    msh_segment[7] = get_current_formatted_date()
+                    msh_segment[8] = ''
+                    msh_segment[9][0][0] = 'ORU'
+                    msh_segment[9][0][1] = 'R01'
+                    msh_segment[10] = get_ticks(datetime.datetime.now())
+
+    def update_orc_segment(self):
+        if self.h and self.h['ORC']:
+            for orc_segment in self.h['ORC']:
+                orc_segment[1] = 'RE'
+
+    def update_obr_segment(self):
+        if self.h and self.h['OBR']:
+            for obr_segment in self.h['OBR']:
+                obr_segment[22] = get_current_formatted_date()
+                obr_segment[25] = 'P'
+                obr_segment[27] = '^^^^^R^^'
+
+    def update_obx_segment(self):
+        if self.h and self.h['OBX']:
+            for obx_segment in self.h['OBX']:
+                obx_segment[2] = 'ST'
+                obx_segment[11] = 'P'
+                obx_segment[14] = get_current_formatted_date()
+                if(len(obx_segment)==19):
+                    obx_segment.append(obx_segment[14])
+                elif(len(obx_segment)>=19):
+                    obx_segment[19] = obx_segment[14]
+   
+    def update_obx_seg_containing_gene(self, sample):
+        temp_obx = self.h[:]
+        l = len(self.h)
+        for i in range(l):
+            del temp_obx[l-i-1]
+        new_obx_index = 1
+        for obxSegment in self.h['OBX']:
+            if obxSegment[3][0][1][0] == "SARS-COV-2, NAA":
+                obxSegment[5][0] = sample.result
+                obxSegment[1] = new_obx_index
+                obxSegment[18][0] = "Genome"
+                new_obx_index +=1 
+                temp_obx.append(obxSegment) 
+            
+        h_t = self.h[:]
+        l = len(self.h)
+        for i in range(l):
+            del h_t[l-i-1]
+        for i in range(len(self.h)):
+            if(self.h[i][0][0]!="OBX"):
+                h_t.append(self.h[i])
+        h_t.extend(temp_obx)
+        return h_t
+
+    def get_first_obx_index(self):
+        idx = 0
+        for seg in self.h:
+            if seg[0][0] == 'OBX':
+                return idx
+            idx += 1
+        return -1
+    # Assuming insertion is just above first OBX segment
+    def update_comments(self, comments):
+        comments_arr = comments.split("\n")
+        obx_idx = self.get_first_obx_index()
+        if (obx_idx == -1):
+            print("OBX segment not found, so appending it")
+            obx_idx = len(self.h) - 1 
+        i=1
+        for comment in comments_arr:
+            self.h.append('NTE|{}|L|{}'.format(i,comment))
+            obx_idx += 1
+            i += 1
+
+
+class Sample:
+    def __init__(self, sample_name, completeSampleName):
+        self.name = str(sample_name)
+        self.nCoV_N1 = None
+        self.nCoV_N2 = None
+        self.nCoV_N3 = None
+        self.RP = None
+        self.result = None
+        self.completeSampleName = completeSampleName
+
+    def __str__(self):
+        return str( self.completeSampleName) + ": " + str(self.nCoV_N1) + " & " + str(self.nCoV_N2) + " & "  + str(self.nCoV_N3) + " & " + str(self.RP) + " & " + str(self.result)
+
+    def __repr__(self):
+        return str( self.completeSampleName) + ": " + str(self.nCoV_N1) + " & " + str(self.nCoV_N2) + " & "  + str(self.nCoV_N3) + " & " + str(self.RP) + " & " + str(self.result) + " | "
